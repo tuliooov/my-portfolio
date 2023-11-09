2ef81d7ba4522ffd4b7530ea051893566bfe7a72
Marco Tulio <marcotuliovaleriano2014@gmail.com>
2023-11-08 22:19:46 -0300

    fix resume

--- src/components/Hero/HeroStyles.ts
+++ src/components/Hero/HeroStyles.ts
@@ -1,5 +1,28 @@
 import styled from 'styled-components'
 
+export const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+  }
+  @media ${(props) => props.theme.breakpoints.md} {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+  }
+
+  > a {
+    margin-bottom: 0;
+  }
+`
+
 export const LeftSection = styled.div`
   width: 100%;
   @media ${(props) => props.theme.breakpoints.sm} {
